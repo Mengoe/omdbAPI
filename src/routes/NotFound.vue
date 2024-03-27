7b7589d7190323d1c6e3170966b2142474443e93
Mengou <gjsehdrh@gmail.com>
2024-03-27 15:46:44 +0900

    ✨ add NotFound component

---
+++ src/routes/NotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="not-found">
+    <div class="status">
+      404
+    </div>
+    <div class="message">
+      Page Not Found!
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '~/scss/main';
+
+.not-found {
+    text-align: center;
+    line-height: 1.2;
+    font-family: 'Oswald', sans-serif;
+    padding : 80px 20px;
+
+    .status {
+        color : $primary;
+        font-size : 160px;
+
+    }
+    .message {
+        font-size:50px;
+    }
+}
+</style>
