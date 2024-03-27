eff7cd98201700d58999654e46a732464fdff1d2
Mengou <gjsehdrh@gmail.com>
2024-03-27 15:34:50 +0900

    ✨ make Loader Component

---
+++ src/components/Loader.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    :style="{
+      width: `${size}rem`,
+      height : `${size}rem`,
+      zIndex : zIndex
+    }"
+    :class="{absolute : absolute, fixed : fixed}"
+    class="spinner-border text-primary"></div>
+</template>
+
+<script>
+    export default {
+        props: {
+            size : {
+                type: Number,
+                default : 2
+            },
+            absolute : {
+                type : Boolean,
+                default : false
+            },
+            fixed : {
+                type : Boolean,
+                default : false
+            },
+            zIndex : {
+                type : Number,
+                default : 0
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.spinner-border { 
+    margin : auto;
+    top : 0;
+    bottom : 0;
+    left : 0;
+    right : 0;
+    // absolute : 부모 요소 기준으로의 배치
+    &.absolute {
+        position : absolute;
+    }
+
+    // fixed : 뷰포트의 특정 위치에 배치
+    &.fixed {
+        position : fixed;
+    }
+}
+
+</style>
