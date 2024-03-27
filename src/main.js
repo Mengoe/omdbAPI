5de975f9746969743478d7f79ad2594ca2857da4
Mengou <gjsehdrh@gmail.com>
2024-03-27 15:38:02 +0900

    ✨ use vue 'loadImage' plugin to improve reusability

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import {createApp} from 'vue'
 import App from './App'
 import router from './routes' // index.js는 생략가능. default로 사용되는 js 파일
 import store from './store/index.js'
+import loadImage from './plugins/loadImage.js'
 
 createApp(App)
     .use(router) // 플러그인 연결
     .use(store)
+    .use(loadImage)
     .mount('#app')
